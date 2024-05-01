0b806410b09763ef16aa9aff48e5a6d3c9bb65d3
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-05-01 08:46:11 +0200

    chore: Adds password confirmation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,6 +10,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
         user: {
           name: 'The name',
           password: '12345678',
+          password_confirmation: '12345678',
           email: 'test@email.com'
         }
       }
@@ -25,11 +26,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get sign_up_path
     assert_response :ok
 
-    assert_no_difference ['User.count', 'Organization.count'], 1 do 
+    assert_no_difference ['User.count', 'Organization.count'] do 
       post sign_up_path, params: {
         user: {
           name: 'The name',
           password: '1234',
+          password_confirmation: '1234',
           email: 'test_email.com'
         }
       }
@@ -38,5 +40,25 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :unprocessable_entity
 
     assert_select 'p.is-danger', text: I18n.t('activerecord.errors.models.user.attributes.password.too_short')
+  end
+
+  test 'render errors if passwords don not match' do
+    get sign_up_path
+    assert_response :ok
+
+    assert_no_difference ['User.count', 'Organization.count'] do 
+      post sign_up_path, params: {
+        user: {
+          name: 'The name',
+          password: '12345678',
+          password_confirmation: '12345',
+          email: 'test@email.com'
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+
+    assert_select 'p.is-danger', text: I18n.t('activerecord.errors.models.user.attributes.password_confirmation.confirmation')
   end
 end
