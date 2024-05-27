3781cccf7aa4aade3d1b1c2c115fbdff1b1587e0
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-05-27 20:40:43 +0200

    test(controllers): Test that reset password email are sent

--- test/controllers/users/password_resets_controller_test.rb
+++ test/controllers/users/password_resets_controller_test.rb
@@ -1,7 +1,15 @@
-require "test_helper"
+require 'test_helper'
 
 class Users::PasswordResetsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:jerry)
+
+    ActionMailer::Base.deliveries.clear
+  end
+  test 'creating a password reset sends an email and and shows instructions' do
+    post users_password_resets_path, params: { email: @user.email }
+    assert_response :ok
+    assert_select 'p', text: I18n.t('users.password_resets.create.message')
+    assert_emails 1
+  end
 end
