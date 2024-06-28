7c4094d041811353e6c38ea46855fcff2210cc00
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-06-28 06:22:57 +0200

    Lint schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,55 +12,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_02_075757) do
+ActiveRecord::Schema[7.1].define(version: 20_240_602_075_757) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "app_sessions", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "token_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_app_sessions_on_user_id"
+  create_table 'app_sessions', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.string 'token_digest'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_app_sessions_on_user_id'
   end
 
-  create_table "listings", force: :cascade do |t|
-    t.string "title"
-    t.integer "price"
-    t.bigint "organization_id", null: false
-    t.bigint "creator_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["creator_id"], name: "index_listings_on_creator_id"
-    t.index ["organization_id"], name: "index_listings_on_organization_id"
+  create_table 'listings', force: :cascade do |t|
+    t.string 'title'
+    t.integer 'price'
+    t.bigint 'organization_id', null: false
+    t.bigint 'creator_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['creator_id'], name: 'index_listings_on_creator_id'
+    t.index ['organization_id'], name: 'index_listings_on_organization_id'
   end
 
-  create_table "memberships", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "organization_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["organization_id"], name: "index_memberships_on_organization_id"
-    t.index ["user_id"], name: "index_memberships_on_user_id"
+  create_table 'memberships', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'organization_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['organization_id'], name: 'index_memberships_on_organization_id'
+    t.index ['user_id'], name: 'index_memberships_on_user_id'
   end
 
-  create_table "organizations", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'organizations', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.string 'password_digest'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
 
-  add_foreign_key "app_sessions", "users"
-  add_foreign_key "listings", "organizations"
-  add_foreign_key "listings", "users", column: "creator_id"
-  add_foreign_key "memberships", "organizations"
-  add_foreign_key "memberships", "users"
+  add_foreign_key 'app_sessions', 'users'
+  add_foreign_key 'listings', 'organizations'
+  add_foreign_key 'listings', 'users', column: 'creator_id'
+  add_foreign_key 'memberships', 'organizations'
+  add_foreign_key 'memberships', 'users'
 end
