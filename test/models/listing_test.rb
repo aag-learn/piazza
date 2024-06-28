05427b1430139545a3029f3292e19f01c13228d0
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-06-28 06:18:11 +0200

    Add test for the Listing model validations
    Exercise in section 9.1.6

--- test/models/listing_test.rb
+++ test/models/listing_test.rb
@@ -1,7 +1,35 @@
-require "test_helper"
+require 'test_helper'
 
 class ListingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @creator = users(:jerry)
+    @organization = organizations(:one)
+  end
+
+  test 'Requires a title' do
+    listing = Listing.new(title: '', price: 1000, creator: @creator, organization: @organization)
+    assert_not listing.valid?
+
+    listing.title = 'A valid title'
+    assert listing.valid?
+  end
+
+  test 'Title should be between 10 and 100 characters' do
+    listing = Listing.new(title: 'A valid title', price: 1000, creator: @creator, organization: @organization)
+    assert listing.valid?
+
+    listing.title = 'The'
+    assert_not listing.valid?
+
+    listing.title = 'x' * 101
+    assert_not listing.valid?
+  end
+
+  test 'Price should be an integer' do
+    listing = Listing.new(title: 'A valid title', price: 1000, creator: @creator, organization: @organization)
+    assert listing.valid?
+
+    listing.price = 1000.5
+    assert_not listing.valid?
+  end
 end
