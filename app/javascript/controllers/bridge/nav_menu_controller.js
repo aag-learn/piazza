49ba8f05ac5d5b93bd666cc1dcbd0228849660e1
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-07-14 11:06:24 +0200

    Add native buttons for listings

--- app/javascript/controllers/bridge/nav_menu_controller.js
+++ app/javascript/controllers/bridge/nav_menu_controller.js
@@ -7,11 +7,17 @@ export default class extends BridgeComponent {
   connect() {
     this.element.classList.add("is-hidden")
 
+    // About the icon property in the itemTargets
+    // Sending the attribute to both apps is slightly unintuitive as we’ll ignore it on Android. We can check the 
+    // current platform via the BridgeElement class, so it’s possible to send platform specific messages by 
+    // namespacing data- attributes and writing some logic to that effect. In this case I’ve chosed to keep it simple,
+    // but I’d recommend making this distinction for bigger applications.
     const items =
       this.itemTargets
         .map(item => new BridgeElement(item))
         .map((item, index) => ({
           title: item.title,
+          icon: item.bridgeAttribute("icon"),
           index
         }))
 
