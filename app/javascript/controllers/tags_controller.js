df170ed70dc469868f1e5dbebd02356d468320ff
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-07-19 07:07:45 +0200

    Tags cannot be empty enymore

--- app/javascript/controllers/tags_controller.js
+++ app/javascript/controllers/tags_controller.js
@@ -6,6 +6,7 @@ export default class extends Controller {
   addTag() { // Called by the "data-action" attribute
     var input = this.inputTarget.value;
     var templateHtml = this.templateTarget.innerHTML;
+    if (!input) { return; }
     templateHtml = templateHtml.replace(/{value}/g, input);
     this.containerTarget.insertAdjacentHTML('beforeend', templateHtml);
     this.inputTarget.value = "";
